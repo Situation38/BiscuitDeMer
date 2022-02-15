955ecc5c74e3f221c3d0ea6ae7362805abf6279e
Situation <77997284+Situation38@users.noreply.github.com>
2022-02-15 14:46:55 +0100

    Add files via upload
    update du programme

---
+++ BiscuitDeMerTest.cs
@@ -0,0 +1,179 @@
+
+using BiscuitDeMer;
+using NUnit.Framework;
+
+namespace TestBiscuitDeMer
+{
+    public class BiscuitDeMerTest
+    {
+                /*Les tests nous conduisent a la redaction dun code clean
+                 * beaucoup plus generique.
+                 * 
+                 * 
+                 * Test Okey    
+                 */
+
+
+
+
+        [Test]
+        public void doisRetourner1SiLeNombreEst1()
+            //on voudrais  que la sortie nous renvoie une chaine de caractere 1
+        {
+            //ARRANGE
+            var bDeMer = new BiscuitDemer();
+
+            //ACT+ASSERT
+            //on a 1 et on veut que en sortie de notre methode genere on est "1"
+            Assert.AreEqual("1", bDeMer.Genere(1,1));
+        }
+
+
+        [Test]
+        public void doisRetourner2SiLeNombreEst2()
+        //on voudrais  que la sortie nous renvoie une chaine de caractere 2
+        {
+            //ARRANGE
+            var bDeMer = new BiscuitDemer();
+
+            //ACT+ASSERT
+            //on a 2 et on veut que en sortie de notre methode genere on est "2"
+            Assert.AreEqual("2", bDeMer.Genere(2,2));
+        }
+
+
+        [Test]
+        public void doisRetourner_Biscuit_SiLeNombreEst3()
+        //on voudrais  que la sortie nous renvoie une chaine de caractere Biscuit
+        {
+            //ARRANGE
+            var bDeMer = new BiscuitDemer();
+
+            //ACT+ASSERT
+            //on a 3 et on veut que en sortie de notre methode genere on est "Biscuit"
+            Assert.AreEqual("Biscuit", bDeMer.Genere(3,3));
+        }
+
+
+
+        [Test]
+        public void  doisRetourner_De_SiLeNombreEst6()
+        //on voudrais  que la sortie nous renvoie une chaine de caractere DeMer
+        {
+            //ARRANGE
+            var bDeMer = new BiscuitDemer();
+
+            //ACT+ASSERT
+            //on a 5 et on veut que en sortie de notre methode genere on est "DeMer"
+            Assert.AreEqual("Biscuit", bDeMer.Genere(6,6));
+        }
+
+        [Test]
+        public void doisRetourner_De_SiLeNombreEst5()
+        //on voudrais  que la sortie nous renvoie une chaine de caractere DeMer
+        {
+            //ARRANGE
+            var bDeMer = new BiscuitDemer();
+
+            //ACT+ASSERT
+            //on a 5 et on veut que en sortie de notre methode genere on est "DeMer"
+            Assert.AreEqual("DeMer", bDeMer.Genere(5,5));
+        }
+
+        [Test]
+        public void doisRetourner_De_SiLeNombreEst10()
+        //on voudrais  que la sortie nous renvoie une chaine de caractere DeMer
+        {
+            //ARRANGE
+            var bDeMer = new BiscuitDemer();
+
+            //ACT+ASSERT
+            //on a 10 et on veut que en sortie de notre methode genere on est "DeMer"
+            Assert.AreEqual("DeMer", bDeMer.Genere(10,10));
+        }
+
+
+        [Test]
+        public void doisRetourner_BiscuitDeMer_SiLeNombreEst15()
+        //on voudrais  que la sortie nous renvoie une chaine de caractere BiscuitDeMer
+        {
+            //ARRANGE
+            var bDeMer = new BiscuitDemer();
+
+            //ACT+ASSERT
+            //on a 15 et on veut que en sortie de notre methode genere on est "BiscuitDeMer"
+            Assert.AreEqual("BiscuitDeMer", bDeMer.Genere(15,15));
+        }
+
+
+        [Test]
+        public void doisRetourner_BiscuitDeMer_SiLeNombreEst30()
+        //on voudrais  que la sortie nous renvoie une chaine de caractere BiscuitDeMer
+        {
+            //ARRANGE
+            var bDeMer = new BiscuitDemer();
+
+            //ACT+ASSERT
+            //on a 30 et on veut que en sortie de notre methode genere on est "BiscuitDeMer"
+            Assert.AreEqual("BiscuitDeMer", bDeMer.Genere(30,30));
+        }
+
+        public void doisRetourner_12_SiLesNombres1_2()
+        //on On passe deux nombres en parametre et on aimerait
+        //quil nous renvoie deux nombres concatenes en sortie
+        {
+            //ARRANGE
+            var bDeMer = new BiscuitDemer();
+
+            //ACT+ASSERT
+            //on a 30 et on veut que en sortie de notre methode genere on est "12"
+
+            Assert.AreEqual("12", bDeMer.Genere(1,2));
+        }
+
+
+        public void doisRetourner_12Biscuit_SiLesNombres1A3()
+        //On passe des nombres dans une sequence en parametre et on aimerait
+        //quil nous renvoie en sortie leurss valeurs en string concatenes
+        {
+            //ARRANGE
+            var bDeMer = new BiscuitDemer();
+
+            //ACT+ASSERT
+            //De 1 a 3 et on veut que en sortie de notre methode genere on est "12Biscuit"
+
+            Assert.AreEqual("12Biscuit", bDeMer.Genere(1, 3));
+        }
+
+        public void doisRetourner_12Biscuit4Demer_SiLesNombres1A5()
+        //On passe des nombres dans une sequence en parametre et on aimerait
+        ///quil nous renvoie en sortie leurs valeurs en string concatenes
+        {
+            //ARRANGE
+            var bDeMer = new BiscuitDemer();
+
+            //ACT+ASSERT
+            //De 1 a 5et on veut que en sortie de notre methode genere on est "BiscuitDeMer"
+            Assert.AreEqual("12Biscuit", bDeMer.Genere(1, 5));
+        }
+
+
+        public void doisRetourner_12Biscuit4DeMerBiscuit78BiscuitDemer11Biscuit1314BiscuitDeMer_SiLesNombres1A15()
+        //On passe des nombres dans une sequence en parametre et on aimerait
+        ///quil nous renvoie en sortie leurs valeurs en string concatenes
+        {
+            //ARRANGE
+            var bDeMer = new BiscuitDemer();
+            //ACT+ASSERT
+            //De 1 a 5et on veut que en sortie de notre methode genere on est "BiscuitDeMer"
+            Assert.AreEqual("12Biscuit4DeMerBiscuit78BiscuitDemer11Biscuit1314BiscuitDeMer"
+                , bDeMer.Genere(1, 15));
+        }
+
+}
+}            
+
+ 
+                      
+                // Tests Okey
+               
